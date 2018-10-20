88ae3d8ee9a6097cd44ade672dee30f233e59718
Rui <33549232+user-cube@users.noreply.github.com>
2018-10-21 00:57:27 +0100

    Add files via upload

---
+++ Kitchen/src/logging/LoggerStub.java
@@ -0,0 +1,102 @@
+package logging;
+
+import commonStructures.ClientCom;
+import commonStructures.Message;
+import entities.states.ChefState;
+import entities.states.StudentState;
+import entities.states.WaiterState;
+import sharedRegions.interfaces.SharedRegionStub;
+
+/**
+ * Stub do Logger - (General Repository of Information)
+ */
+public class LoggerStub extends SharedRegionStub implements Logger {
+
+    /**
+     * Instanciação do logger (General Repository of Information)
+     *
+     * @param hostName nome do computador onde o server está alojado.
+     * @param port     porta em que o servidor se encontra à escuta.
+     */
+    public LoggerStub(String hostName, int port) {
+        super(hostName, port);
+    }
+
+
+    @Override
+    public void updateWaiterState(WaiterState state) {
+        ClientCom clientCom = createAndOpenConnection();
+        Message   outMessage;
+
+        outMessage = new Message(Message.Type.UPDATE_WAITER)
+                .setWaiterState(state);
+        clientCom.writeObject(outMessage);
+        waitForMessage(clientCom, Message.Type.LOGGER_MESSAGE_RECEIVED);
+        clientCom.close();
+    }
+
+
+    @Override
+    public void updateStudentState(long studentId, StudentState state) {
+        ClientCom clientCom = createAndOpenConnection();
+        Message   outMessage;
+        outMessage = new Message(Message.Type.UPDATE_STUDENT)
+                .setStudentState(state).setStudentId(studentId);
+        clientCom.writeObject(outMessage);
+        waitForMessage(clientCom, Message.Type.LOGGER_MESSAGE_RECEIVED);
+        clientCom.close();
+    }
+
+    @Override
+    public void updateChefState(ChefState state) {
+        ClientCom clientCom = createAndOpenConnection();
+        Message   outMessage;
+        outMessage = new Message(Message.Type.UPDATE_CHEF)
+                .setChefState(state);
+        clientCom.writeObject(outMessage);
+        waitForMessage(clientCom, Message.Type.LOGGER_MESSAGE_RECEIVED);
+        clientCom.close();
+    }
+
+    @Override
+    public void updateCourse(int numberCourse) {
+        ClientCom clientCom = createAndOpenConnection();
+        Message   outMessage;
+        outMessage = new Message(Message.Type.UPDATE_COURSE)
+                .setNumberCourse(numberCourse);
+        clientCom.writeObject(outMessage);
+        waitForMessage(clientCom, Message.Type.LOGGER_MESSAGE_RECEIVED);
+        clientCom.close();
+    }
+
+    @Override
+    public void kitchenFinished() {
+        ClientCom clientCom = createAndOpenConnection();
+        Message   outMessage;
+        outMessage = new Message(Message.Type.KITCHEN_FINISHED);
+        clientCom.writeObject(outMessage);
+        waitForMessage(clientCom, Message.Type.LOGGER_MESSAGE_RECEIVED);
+        clientCom.close();
+    }
+
+    @Override
+    public void barFinished() {
+        ClientCom clientCom = createAndOpenConnection();
+        Message   outMessage;
+        outMessage = new Message(Message.Type.BAR_FINISHED);
+        clientCom.writeObject(outMessage);
+        waitForMessage(clientCom, Message.Type.LOGGER_MESSAGE_RECEIVED);
+        clientCom.close();
+    }
+
+    @Override
+    public void tableFinished() {
+        ClientCom clientCom = createAndOpenConnection();
+        Message   outMessage;
+        outMessage = new Message(Message.Type.TABLE_FINISHED);
+        clientCom.writeObject(outMessage);
+        waitForMessage(clientCom, Message.Type.LOGGER_MESSAGE_RECEIVED);
+        clientCom.close();
+    }
+
+}
---
+++ Waiter/src/entities/states/WaiterState.java
@@ -0,0 +1,31 @@
+package entities.states;
+
+/**
+ * Estados do Waiter.
+ */
+public enum WaiterState {
+    APPRAISING_SITUATION("APPR"),
+    PRESENTING_MENU("PRESENT"),
+    TAKING_ORDER("TAKE"),
+    PLACING_ORDER("PLACE"),
+    WAITING_FOR_PORTION("WAIT_PORT"),
+    PROCESSING_BILL("PROCESS"),
+    RECEIVING_PAYMENT("RECEIVE");
+
+    private final String id;
+
+    WaiterState(String id) {
+        this.id = id;
+    }
+
+    /**
+     * Passagem dos estados do Waiter para String.
+     *
+     * @return string do estado do Waiter.
+     */
+    @Override
+    public String toString() {
+        return id;
+    }
+
+}
